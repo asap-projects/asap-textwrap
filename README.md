f3928ecaf0c9cd8d6336a601e1dbb592a9993a0e
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2022-02-18 22:18:36 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Starter project for C++ with cmake
+
+![Start Now!!](doc/_static/logo.png "ASAP Logo")
+
+[![Linux Build (Ubuntu latest)](https://github.com/abdes/asap/actions/workflows/linux-build.yml/badge.svg?branch=develop)](https://github.com/abdes/asap/actions/workflows/linux-build.yml)
+[![Windows Build (latest)](https://github.com/abdes/asap/actions/workflows/windows-build.yml/badge.svg?branch=develop)](https://github.com/abdes/asap/actions/workflows/windows-build.yml)
+
+## [Project Documentation](https://abdes.github.io/asap/)
+
+## Overview
+
+- `CMake` as the build system with or without presets
+- cross-platform portability on Linux, OS X and Windows
+- multiple compilers: clang, g++ and MSVC
+- modular structure with each module self-contained in a subdirectory within the project
+- `CMake` build helpers to facilitate declaration of library, exe, test modules, for the
+  end-to-end lifecycle including doc generation, test, packaging etc...
+- common facilities (common module) for platform specifics, assertions support, logging
+- unit testing with Google Test
+- code coverage with clang or g++
+- zero-touch valgrind, clang-tidy, clang-format, google sanitizers, etc.
+- development can be done locally or in a dev container with vscode.
+
+## Getting the code
+
+```bash
+git clone --recurse-submodules -j4 https://github.com/abdes/asap.git
+```
+
+NOTES:
+
+- -j4 requests git to parallelize cloning of repos. Needs a relatively recent version of git. If
+  that is not available, simply do not use this option.
+
+## Requirements
+
+Make sure you have a C++ compiler with C++-17 capabilities at least. Gnu, Clang and MSVC all can do
+that with a recent version.
+
+## Enabling husky/commitlint/standard-version
+
+Only one time after the project is cloned, do the following:
+
+```bash
+npx husky install
+npm install -g @commitlint/cli @commitlint/config-conventional
+npm install -g standard-version
+```
+
+## Building
+
+```bash
+mkdir _build && cd _build && cmake .. && cmake --build .
+```
+
+or just use one of the predefined `CMake` presets. Detailed instructions are in the project
+documentation, and many useful commands are listed [here](https://abdes.github.io/asap/master/html/01-getting-started/useful-commands.html).
+
+
+```cmake
+# Project options
+option(BUILD_SHARED_LIBS        "Build shared instead of static libraries."              ON)
+option(ASAP_BUILD_TESTS         "Build tests."                                           OFF)
+option(ASAP_BUILD_EXAMPLES      "Build examples."                                        OFF)
+option(ASAP_WITH_GOOGLE_ASAN    "Instrument code with address sanitizer"                 OFF)
+option(ASAP_WITH_GOOGLE_UBSAN   "Instrument code with undefined behavior sanitizer"      OFF)
+option(ASAP_WITH_GOOGLE_TSAN    "Instrument code with thread sanitizer"                  OFF)
+option(ASAP_WITH_VALGRIND       "Builds targets with valgrind profilers added"           OFF)
+```
---
+++ cmake/SphinxGeneration.cmake
@@ -0,0 +1,82 @@
+# ===-----------------------------------------------------------------------===#
+# Distributed under the 3-Clause BSD License. See accompanying file LICENSE or
+# copy at https://opensource.org/licenses/BSD-3-Clause).
+# SPDX-License-Identifier: BSD-3-Clause
+# ===-----------------------------------------------------------------------===#
+
+# ------------------------------------------------------------------------------
+# Documentation with Sphinx/Breathe/Exhale
+# ------------------------------------------------------------------------------
+
+# To avoid indiscriminately generating documentation for all modules in the
+# project, including third party modules and stuff for which we don't want
+# documentation to be generated, we provide here the basic tools to add
+# breathe/exhale capabilities to a module. Additionally, exhale can only process
+# one module at a time, so we need to have the generation run for each module.
+#
+# Doxygen is run separately NOT by exhale. Sphinx targets must be run after
+# doxygen.
+#
+# To use in a submodule or in the master module add the following:
+#
+# ~~~
+#      asap_with_sphinx(${target})
+# ~~~
+#
+# Use 'make sphinx' to generate documentation. (not done by default)
+#
+
+include(FindSphinx)
+
+if(SPHINX_FOUND)
+  message(STATUS "System has sphinx.")
+
+  # Add a master sphinx target to collect all submodules
+  add_custom_target(sphinx)
+  set_target_properties(sphinx PROPERTIES EXCLUDE_FROM_ALL TRUE)
+
+  # The macro to add a submodule as a sphinx target.
+  macro(asap_with_sphinx TARGET_NAME)
+    # Setup work directory for the target module
+    set(SPHINX_TARGET_WORKDIR "${SPHINX_BUILD_DIR}/${TARGET_NAME}")
+    if(NOT EXISTS "${SPHINX_TARGET_WORKDIR}")
+      file(MAKE_DIRECTORY "${SPHINX_TARGET_WORKDIR}")
+    endif()
+    # Do @ substitutions in the sphinx config file for the module
+    configure_file("${CMAKE_CURRENT_SOURCE_DIR}/doc/conf.py.in"
+                   "${CMAKE_CURRENT_SOURCE_DIR}/doc/conf.py" @ONLY)
+
+    # Add a target for building the sphinx documentation of the module
+    add_custom_target(
+      ${TARGET_NAME}_sphinx
+      COMMAND
+        ${SPHINX_EXECUTABLE} -q -b html -c "${EXHALE_TARGET_WORKDIR}" -d
+        "${SPHINX_TARGET_WORKDIR}/_doctrees" "${CMAKE_CURRENT_SOURCE_DIR}/doc"
+        "${SPHINX_TARGET_WORKDIR}/html"
+      WORKING_DIRECTORY "${SPHINX_TARGET_WORKDIR}"
+      VERBATIM
+      COMMENT "Generating `sphinx` documentation for `${TARGET_NAME}`")
+    set_target_properties(${TARGET_NAME}_sphinx PROPERTIES EXCLUDE_FROM_ALL
+                                                           TRUE)
+    # Finally add the module sphinx target as a dependency for the overall
+    # sphinx target
+    add_dependencies(sphinx ${TARGET_NAME}_sphinx)
+
+    # Install sphinx docs
+    install(
+      DIRECTORY ${SPHINX_BUILD_DIR}/${TARGET_NAME}
+      DESTINATION ${ASAP_INSTALL_DOC}
+      COMPONENT ${TARGET_NAME}_docs)
+
+  endmacro()
+  # We only build documentation through explicit invocation of the sphinx target
+  # as it is pretty heavy and requires doxygen to be run before it is invoked.
+  set_target_properties(sphinx PROPERTIES EXCLUDE_FROM_ALL TRUE)
+
+else(SPHINX_FOUND)
+  message(STATUS "WARNING: sphinx is not available on this system!")
+
+  macro(asap_with_sphinx TARGET_NAME)
+
+  endmacro()
+endif(SPHINX_FOUND)
