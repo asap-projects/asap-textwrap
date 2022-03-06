7390edee47825fbda7a66c72a48942e41a71404f
abdes <abdes@users.noreply.github.com>
2022-03-06 11:10:35 +0000

    deploy: a784a5b3c0251c940e2b2980784fd063bd6d09f7

--- asap_textwrap_master/html/_static/documentation_options.js
+++ asap_textwrap_master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0 (50f3c8d4181d)',
+    VERSION: '1.0.0 (a784a5b3c025)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
