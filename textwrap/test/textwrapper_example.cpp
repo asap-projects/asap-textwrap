e7ea8b2437d709f1dea1a519e00acbd0e18f84ea
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2023-04-14 18:28:23 +0400

    test: enhance example to include more features
    Made some small changes to the input text to demonstrate additional
    capabilities of the library such as line breaks management and trimming
    of white spaces from lines.

--- textwrap/test/textwrapper_example.cpp
+++ textwrap/test/textwrapper_example.cpp
@@ -13,26 +13,26 @@ namespace asap::wrap {
 namespace {
 
 // NOLINTNEXTLINE
-TEST(TextWrapper, WrapExample) {
+TEST(TextWrapperExamples, TypicalWrapWithLineBreaksAndCollapsedSpaces) {
   //! [Example usage]
-  const auto *book = "Pride and Prejudice:";
-  const auto *passage = "It is a truth universally acknowledged, that a single "
-                        "man in possession of a good fortune, must be in want "
-                        "of a wife.";
+  const auto *passage =
+      "  Pride and Prejudice:\n    It is a truth universally   "
+      "acknowledged, that a single man in possession of a "
+      "good fortune, must be in want "
+      "of a wife.   ";
 
   constexpr size_t column_width = 28;
-  const TextWrapper title_wrapper = TextWrapper::Create().Width(column_width);
-  const TextWrapper text_wrapper = TextWrapper::Create()
-                                       .Width(column_width)
-                                       .TrimLines()
-                                       .ReplaceWhiteSpace()
-                                       .CollapseWhiteSpace()
-                                       .IndentWith()
-                                       .Initially("   ")
-                                       .Then("   ");
+  const TextWrapper wrapper = TextWrapper::Create()
+                                  .Width(column_width)
+                                  .TrimLines()
+                                  .ReplaceWhiteSpace()
+                                  .CollapseWhiteSpace()
+                                  .IndentWith()
+                                  .Initially("")
+                                  .Then("...");
 
-  std::cout << title_wrapper.Fill(book).value_or("error") << std::endl;
-  std::cout << text_wrapper.Fill(passage).value_or("error") << std::endl;
+  std::cout << "|" << wrapper.Fill(passage).value_or("error") << "|"
+            << std::endl;
 
   // Pride and Prejudice:
   //    It is a truth universally
